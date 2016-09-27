a382722337ad8b53fa22c35a3cd0492770d3e202
Yang Jae Kang <kangyangjae@gmail.com>
2016-09-27 13:03:27 +0000

    update

--- 1.RNAseq.bam.visualization.gffindexing.py
+++ 1.RNAseq.bam.visualization.gffindexing.py
@@ -4,15 +4,16 @@ import pandas as pd
 
 
 file_gff = sys.argv[1] # gff file '/ref/Cre/DroughtNet/PhytozomeV10_download/References/Alyrata/annotation/Alyrata_107_v1.0.gene_exons.gff3'
-df_gff   = pd.read_csv(file_gff,sep='\t',skiprows=2,header=None)
-
-# For CRE
-#df_gff['genename'] = df_gff[8].apply(lambda x : '.'.join(x.split(';')[0].replace('ID=','').split('.')[0:2]))
-#df_gff['transcriptname'] = df_gff[8].apply(lambda x : '.'.join(x.split(';')[0].replace('ID=','').split('.')[0:4]))
-
+df_gff   = pd.read_csv(file_gff,sep='\t',comment='#',header=None)
 
 dicT2G      = {} # transcript to genename
 df_gff_mRNA = df_gff[(df_gff[2] == 'mRNA')]
+
+def infoparse(x):
+    key   = [i.split('=')[0] for i in x.split(';')]
+    value = [i.split('=')[1] for i in x.split(';')]
+    return dict(zip(key,value))
+
 def get_genename(x):
     if x[2] == 'gene':
         return None
@@ -21,8 +22,6 @@ def get_genename(x):
     dic   = dict(zip(key,value))
     return dic['Parent']
 def get_transcript(x):
-    
-    
     key   = [y.split('=')[0] for y in x[8].split(';')]
     value = [y.split('=')[1] for y in x[8].split(';')]
     dic   = dict(zip(key,value))
@@ -34,8 +33,7 @@ def get_transcript(x):
         return dic['Parent']
 df_gff_mRNA['genename']   = df_gff_mRNA.apply(get_genename,axis=1)
 df_gff_mRNA['transcript'] = df_gff_mRNA.apply(get_transcript,axis=1)
-df_gff_mRNA_ix = df_gff_mRNA.set_index('transcript')
-# For ESA
+df_gff_mRNA_ix            = df_gff_mRNA.set_index('transcript')
 df_gff['transcriptname']  = df_gff.apply(get_transcript,axis=1)
 
 
@@ -47,11 +45,8 @@ def get_genename2(x):
         return None
 df_gff['genename'] = df_gff['transcriptname'].apply(get_genename2)
 
-
-
 # grep longest transcript names
-df_gff_mRNA_ix['longest'] = df_gff_mRNA_ix[8].apply(lambda x : x.split(';')[3].replace('longest=',''))
-#df_gff_mRNA_index =  df_gff_mRNA.set_index('transcript')
+df_gff_mRNA_ix['longest'] = df_gff_mRNA_ix[8].apply(lambda x : infoparse(x)['longest'])
 
 def get_longest(x):
     if x == None:
@@ -61,9 +56,6 @@ def get_longest(x):
     except KeyError:
         return None
 df_gff['longest'] = df_gff['transcriptname'].apply(get_longest)
-
-df_gff_index = df_gff.set_index(['genename','longest'])
-
+df_gff_index      = df_gff.set_index(['genename','longest'])
 df_gff_index.to_pickle(file_gff+'.pandas.df.pk')
 
-#df_gff_index.head()
